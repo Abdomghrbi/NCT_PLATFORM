02632bf1972dc33acfbba37ab64a8fd42a202155
Abdullrahman almaghrabi <73081830+Abdomghrbi@users.noreply.github.com>
2026-04-19 23:27:01 +0300

    Update MainScreen.jsx

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -182,7 +182,8 @@ function MainScreen({ language, user }) {
             position: 'relative',
             cursor: 'pointer',
             transform: showTap ? 'scale(0.95) translateY(10px)' : 'scale(1)',
-              filter: 'drop-shadow(0 25px 50px rgba(0,0,0,0.4))'
+             transition: 'all 0.1s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+           filter: 'drop-shadow(0 25px 50px rgba(0,0,0,0.4))'
   
           }}
         >
